afe77dba5bbb56ba926403a5da7bee900d4d95d6
lia-oliveira <dev.gillian@gmail.com>
2025-01-29 22:28:40 -0300

    User class mapped

--- backend/src/main/java/com/oliveiralia/backend/entities/User.java
+++ backend/src/main/java/com/oliveiralia/backend/entities/User.java
@@ -2,9 +2,20 @@ package com.oliveiralia.backend.entities;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	private String email;
 	private String phone;
