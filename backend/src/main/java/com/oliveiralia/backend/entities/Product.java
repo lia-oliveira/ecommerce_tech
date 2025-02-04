6c9924747754773a8ad6c3efd92912743163aba8
lia-oliveira <dev.gillian@gmail.com>
2025-02-04 18:08:25 -0300

    many-to-many relationship with association class

--- backend/src/main/java/com/oliveiralia/backend/entities/Product.java
+++ backend/src/main/java/com/oliveiralia/backend/entities/Product.java
@@ -1,10 +1,12 @@
 package com.oliveiralia.backend.entities;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -32,6 +34,9 @@ public class Product {
 	           joinColumns = @JoinColumn(name = "product_id"), 
 	           inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {}
 
@@ -85,6 +90,14 @@ public class Product {
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	public List<Order> getOrders() {
+		return items.stream().map(x -> x.getOrder()).toList();
 	}
 	
 	
