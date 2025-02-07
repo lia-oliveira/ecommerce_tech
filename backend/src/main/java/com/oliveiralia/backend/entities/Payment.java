d9ab6630ba3ca245bfe8736123fce24dc573f67c
lia-oliveira <dev.gillian@gmail.com>
2025-02-06 23:17:06 -0300

    equals and hashcode

--- backend/src/main/java/com/oliveiralia/backend/entities/Payment.java
+++ backend/src/main/java/com/oliveiralia/backend/entities/Payment.java
@@ -1,6 +1,7 @@
 package com.oliveiralia.backend.entities;
 
 import java.time.Instant;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -53,6 +54,23 @@ public class Payment {
 
 	public Order getOrder() {
 		return order;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
