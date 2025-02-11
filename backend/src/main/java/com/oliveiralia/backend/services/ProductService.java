1729832abf37638f4f21fabf40813af82fb5b7bf
lia-oliveira <dev.gillian@gmail.com>
2025-02-11 10:40:12 -0300

    Get products paged

--- backend/src/main/java/com/oliveiralia/backend/services/ProductService.java
+++ backend/src/main/java/com/oliveiralia/backend/services/ProductService.java
@@ -3,6 +3,8 @@ package com.oliveiralia.backend.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,11 +27,10 @@ public class ProductService {
 		return dto;		
 	}
 	
-	//Forma resumida do código acima
-	/*@Transactional(readOnly = true)
-	public ProductDTO findById(Long id) {
-		Product product = repository.findById(id).get();
-		return new ProductDTO(product);		
-	}*/
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> result = repository.findAll(pageable);
+		return result.map(x -> new ProductDTO(x));
+	}
 
 }
