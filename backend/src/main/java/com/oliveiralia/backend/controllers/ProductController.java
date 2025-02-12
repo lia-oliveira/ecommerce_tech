0e82622cd496e992abc40a8f5a92f5f29826998d
lia-oliveira <dev.gillian@gmail.com>
2025-02-12 09:48:48 -0300

    Product update

--- backend/src/main/java/com/oliveiralia/backend/controllers/ProductController.java
+++ backend/src/main/java/com/oliveiralia/backend/controllers/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,10 +25,6 @@ public class ProductController {
 	@Autowired
 	private ProductService service;
 	
-	/*@GetMapping(value = "/{id}")
-	public ProductDTO findById(@PathVariable Long id) {
-		return service.findById(id);
-	}*/
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
@@ -35,26 +32,27 @@ public class ProductController {
 		return ResponseEntity.ok(dto);
 	}
 	
-	/*@GetMapping()
-	public Page<ProductDTO> findAll(Pageable pageable) {
-		return service.findAll(pageable);
-	}*/
+	
 	@GetMapping()
 	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
 		Page<ProductDTO> dto = service.findAll(pageable);
 		return ResponseEntity.ok(dto);
-	}
+	}	
 	
-	/*@PostMapping
-	public ProductDTO insert(@RequestBody ProductDTO dto) {
-		return service.insert(dto);		
-	}*/
+	
 	@PostMapping
 	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);		
+	}
+	
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok(dto);
 	}
 
 }
